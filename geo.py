31e544096cd8ca5b4c534cedf0f13731480e6d2b
KashF <Kashfarhadi@gmail.com>
2019-07-18 23:25:35 -0400

    finished geo functions

---
+++ geo.py
@@ -0,0 +1,15 @@
+def square(n):
+    """return square of the number"""
+    return n * n
+
+def cube(n):
+    """return cube of the number"""
+    return n*n*n
+
+def triangle(n):
+    """return triangle of the number"""
+    sum = 0 
+    for i in range(n, 0, -1):
+        sum = sum + 1
+    return sum
+    
