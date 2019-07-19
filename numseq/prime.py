1a41c1d20c16228425e33ffd56b95dec02872290
KashF <Kashfarhadi@gmail.com>
2019-07-18 23:40:41 -0400

    Redid prime.py, IT WORKSgit add .

--- numseq/prime.py
+++ numseq/prime.py
@@ -1,13 +1,20 @@
-import math
-
-def is_prime(m):
-    m = math.floor(math.sqrt(m)) + 1
-    for i in range(2, n):
-        if(m % i == 0):
-            return False
-        return True
+def is_prime(num):
+    """returns a list of all primes less than the number"""
     
-def primes(n):
-    return[i for i in range[2, n] if is_prime(i)]
+    if num >= 2:
+        for x in range(2, num):
+            if not (num % x):
+                return False
+    else:
+        return False
+    return True
+       
 
+def primes(num):
+    """returns a boolean indicating whether the number is prime"""
+    prime_list = []
+    for number in range(num):
+        if is_prime(number):
+            prime_list.append(number)
+    return prime_list
     
